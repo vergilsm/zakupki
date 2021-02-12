51c29a2070f1b0a4c33c1f91f69e63ea34867dba
vergil <vergil.sm@yandex.ru>
2021-02-12 13:40:48 +0300

    refactoring code

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source 'https://rubygems.org'
 
 gem 'sequel'
 gem 'sqlite3'
+gem 'nokogiri'
+gem 'rubyzip', '>= 1.0.0'
--- search_contract.rb
+++ search_contract.rb
@@ -5,58 +5,62 @@ require 'zip'
 require 'nokogiri'
 
 DB = Sequel.connect('sqlite://db/zakupki.sqlite3')
-@ftp = Net::FTP.new('ftp.zakupki.gov.ru')
-@ftp.login 'free', 'free'
+ftp = Net::FTP.new('ftp.zakupki.gov.ru')
+ftp.login 'free', 'free'
 
-def db_schema
-  DB.create_table? :regions do
-    primary_key :id
-    String :name
-  end
+DB.create_table? :regions do
+  primary_key :id
+  String :name
 end
 
-db_schema
-@regions = DB[:regions]
+regions = DB[:regions]
 
-def list_region
-  @ftp.chdir('/fcs_regions')
-  files = @ftp.list.first(88)
+if regions.empty?
+  ftp.chdir('/fcs_regions')
+  files = ftp.list.first(88)
   all_regions = files.map { |reg| reg.split(" ").last }
   ftp_regions = all_regions.reject { |reg| reg == "ERUZ" || reg == "PG-PZ" }
 
   ftp_regions.each do |region|
-    @regions.insert(name: region)
+    regions.insert(name: region)
   end
 end
 
-list_region if @regions.empty?
+regions.each { |region| puts "number: #{region[:id]} name: #{region[:name]}" }
 
-def get_list_zip(date_user_input, region_user_input, desired_date)
-  region = nil
-  current_month = Time.now.strftime("%m")
+puts "Введите номер региона"
+region_user_input = STDIN.gets.chomp
 
-  @regions.each do |reg|
-    region = reg[:name] if reg[:id] == region_user_input.to_i
-  end
+puts "Введите дату пример: 02-02-2021"
+date_user_input = STDIN.gets.chomp
+desired_date = Date.parse(date_user_input).strftime("%d%m%Y")
 
-  if current_month == Date.parse(date_user_input).strftime("%m")
-    @ftp.chdir("/fcs_regions/#{region}/contracts/currMonth/")
-  else
-    @ftp.chdir("/fcs_regions/#{region}/contracts/")
-  end
+region = nil
+current_month = Time.now.strftime("%m")
 
-  list_file = @ftp.nlst("*.zip").select do |zip_file|
-    desired_date == Date.parse(@ftp.mtime(zip_file).to_s).strftime("%d%m%Y")
-  end.reject { |file| file.to_s.empty? }
-
-  list_file.each do |file|
-    localdir = File.join("tmp/#{date_user_input}/contracts/#{region_user_input}/zip/", file)
-    FileUtils.mkdir_p(File.dirname(localdir))
-    @ftp.getbinaryfile(file, localdir, 1024)
-  end
+regions.each do |reg|
+  region = reg[:name] if reg[:id] == region_user_input.to_i
 end
 
-def get_list_xml(file, date_user_input, region_user_input)
+if current_month == Date.parse(date_user_input).strftime("%m")
+  ftp.chdir("/fcs_regions/#{region}/contracts/currMonth/")
+else
+  ftp.chdir("/fcs_regions/#{region}/contracts/")
+end
+
+list_file = ftp.nlst("*.zip").select do |zip_file|
+  desired_date == Date.parse(ftp.mtime(zip_file).to_s).strftime("%d%m%Y")
+end.reject { |file| file.to_s.empty? }
+
+list_file.each do |file|
+  localdir = File.join("tmp/#{date_user_input}/contracts/#{region_user_input}/zip/", file)
+  FileUtils.mkdir_p(File.dirname(localdir))
+  ftp.getbinaryfile(file, localdir, 1024)
+end
+
+list_zip = Dir["tmp/#{date_user_input}/contracts/#{region_user_input}/zip/*.zip"]
+
+list_zip.each do |file|
   destination = "./tmp/#{date_user_input}/contracts/#{region_user_input}/xml/"
   FileUtils.mkdir_p(destination)
 
@@ -70,36 +74,17 @@ def get_list_xml(file, date_user_input, region_user_input)
   end
 end
 
-def get_list_contract(list_xml)
-  list_xml.each do |file|
-    doc = Nokogiri::XML(File.open(file))
-    doc.css('//regNum').each do |num|
-      puts num.text if num.text.scan(/\d/).length >= 19
-    end
+all_xml_list = Dir["tmp/#{date_user_input}/contracts/#{region_user_input}/xml/*.xml"]
+list_xml = all_xml_list.select { |file| file if file.include?("contractProcedure_")  }
+
+list_xml.each do |file|
+  doc = Nokogiri::XML(File.open(file))
+  if doc.css("//currentContractStage").text == "ET" &&
+     doc.css("//termination reason code").text == "3"
+    puts doc.css("//termination docTermination name").text
+    puts doc.css("//regNum").text
   end
 end
-
-@regions.each { |region| puts "number: #{region[:id]} name: #{region[:name]}" }
-
-puts "Введите номер региона"
-region_user_input = STDIN.gets.chomp
-
-puts "Введите дату пример: 02-02-2021"
-date_user_input = STDIN.gets.chomp
-desired_date = Date.parse(date_user_input).strftime("%d%m%Y")
-
-get_list_zip(date_user_input, region_user_input, desired_date)
-list_zip = Dir["tmp/#{date_user_input}/contracts/#{region_user_input}/zip/*.zip"]
-
-list_zip.each do |file|
-  get_list_xml(file, date_user_input, region_user_input)
-end
-
-list_xml = Dir["tmp/#{date_user_input}/contracts/#{region_user_input}/xml/*.xml"]
-list_xml.delete_if { |f| f.include?("contractProcedure_") }
-list_xml.delete_if { |f| f.include?("contractAvailableForElAct") }
-
-get_list_contract(list_xml)
 
 #list_zip.reject{|file| File.directory?(file) }.each{ |f| File.delete(f) }
 #list_xml = Dir["tmp/#{date_user_input}/contracts/#{region_user_input}/xml/*.xml"]
